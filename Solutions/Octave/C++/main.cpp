2cb25a1ae4e91071f4705bdb8ad26e13cb650673
Rushabh <20rush09abh2001@gmail.com>
2026-01-30 16:02:27 +0530

    Added C++ solution to 'Octave'

---
+++ Solutions/Octave/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Octave
+// Link to the problem: https://atcoder.jp/contests/abc440/tasks/abc440_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << x * pow(2, y) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
