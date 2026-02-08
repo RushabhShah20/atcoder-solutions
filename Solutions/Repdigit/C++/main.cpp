c57176a4ad1c196ad4a4d7a409637ad092dd06b5
Rushabh <20rush09abh2001@gmail.com>
2026-02-08 11:01:48 +0530

    Added C++ solution to 'Repdigit'

---
+++ Solutions/Repdigit/C++/main.cpp
@@ -0,0 +1,28 @@
+// Problem: Repdigit
+// Link to the problem: https://atcoder.jp/contests/abc444/tasks/abc444_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s[0] == s[1] && s[1] == s[2])
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
