1d029906c334eecc71aa490dd50db4c43b14b578
Rushabh <20rush09abh2001@gmail.com>
2026-02-08 11:03:55 +0530

    Added C++ solution to 'Append s'

---
+++ Solutions/Append s/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Append s
+// Link to the problem: https://atcoder.jp/contests/abc443/tasks/abc443_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    s += 's';
+    cout << s << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
