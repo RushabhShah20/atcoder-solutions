4de35c1bc7f46048e6e0ce66fc57f1107a877809
Rushabh <20rush09abh2001@gmail.com>
2026-02-15 09:06:25 +0530

    Added C++ solution to 'Strong Word'

---
+++ Solutions/Strong Word/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Strong Word
+// Link to the problem: https://atcoder.jp/contests/abc445/tasks/abc445_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s.front() == s.back())
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
