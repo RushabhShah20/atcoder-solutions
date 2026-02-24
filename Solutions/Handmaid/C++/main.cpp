d80eb85f830aa3df877462d0a15fe99a293486df
Rushabh <20rush09abh2001@gmail.com>
2026-02-24 16:29:18 +0530

    Added C++ solution to 'Handmaid'

---
+++ Solutions/Handmaid/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Handmaid
+// Link to the problem: https://atcoder.jp/contests/abc446/tasks/abc446_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    s.front() = tolower(s.front());
+    s.insert(0, "Of");
+    cout << s << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
