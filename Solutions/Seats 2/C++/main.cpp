272bebc0a98127ab2100736ea29c5401f5a8aae9
Rushabh <20rush09abh2001@gmail.com>
2026-03-01 10:41:45 +0530

    Added C++ solution to 'Seats 2'

---
+++ Solutions/Seats 2/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Seats 2
+// Link to the problem: https://atcoder.jp/contests/abc447/tasks/abc447_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    if ((n + 1) / 2 >= m)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
