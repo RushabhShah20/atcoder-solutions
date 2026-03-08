2c1bcfd12dc702acdcaa71c3e952a2d448edd261
Rushabh <20rush09abh2001@gmail.com>
2026-03-08 17:48:39 +0530

    Added C++ solution to 'chmin'

---
+++ Solutions/chmin/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: chmin
+// Link to the problem: https://atcoder.jp/contests/abc448/tasks/abc448_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] < x)
+        {
+            x = a[i];
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << 0 << endl;
+        }
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
