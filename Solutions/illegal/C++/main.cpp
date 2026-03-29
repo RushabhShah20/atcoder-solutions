56d6c1ae5a30d6e2876f761792d80148f1f27ce8
Rushabh <20rush09abh2001@gmail.com>
2026-03-29 15:47:30 +0530

    Added C++ solution to 'illegal'

---
+++ Solutions/illegal/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: illegal
+// Link to the problem: https://atcoder.jp/contests/abc451/tasks/abc451_a
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s.size() % 5 == 0)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
